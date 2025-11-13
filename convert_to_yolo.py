9c9c71ae0252b8afbcbbb6d62407b9b2efb500ee
MariaOpov <your_email@example.com>
2025-11-13 16:16:11 +0700

    Them file convert_to_yolo.py va ghi chu

---
+++ convert_to_yolo.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import scipy.io
+import os
+
+# Cần hoàn thành check_mat.py
+# Sau check_mat.py, giúp ta hiểu được dữ liệu (bb chứa tọa độ img_id, xmin, xmax, ymin, ymax)
+# Nhiệm vụ của convert_to_yolo sẽ là:
+# Tải toàn bộ đáp án từ BoundingBox.mat
+# Đọc file ảnh .png tương ứng để lấy kích thước (chiều rộng, chiều cao) thật của ảnh.
+# Thực hiện phép toán để chuyển đổi tọa độ pixel sang định dạng YOLO 
+# Lưu kết quả đã chuyển đổi vào một file .txt mới
+# Nói chung, Nó tạo ra dữ liệu sạch (các file .txt) để chuẩn bị cho mô hình AI (YOLO).
